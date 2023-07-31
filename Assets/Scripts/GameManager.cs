5e120546716bba5e650459ad36014c2774da4b15
fracchiomon <70598103+fracchiomon@users.noreply.github.com>
2023-07-31 16:14:20 +0200

    update MainMenu e livelli vuoti
    messi dei livelli vuoti per testare i buttons dal main menu; in ogni livello e` gia` presente il button per tornare al menu principale. E` possibile usare questi livelli vuoti per costruirci sopra i minigame

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Creazione del Singleton del GameManager che si occupa della gestione delle transizione tra scene e meccaniche piu` importanti
+/// </summary>
 public class GameManager : MonoBehaviour
 {
     protected static GameManager Instance;
-    
+
     protected GameManager()
-    {   }
+    { }
 
     private void Awake()
     {
         if(Instance == null)
         {
-            Instance = this;
+            Instance = this;  
             DontDestroyOnLoad(gameObject);
         }
         else if(Instance != null && Instance != this)
@@ -27,5 +31,40 @@ public class GameManager : MonoBehaviour
     {
         return Instance;
     }
+
+    public static void ToMainMenu()
+    {
+        SceneManager.LoadScene(sceneName: "MainMenu");
+    }
+    public static void ToLevel1()
+    {
+        SceneManager.LoadScene(sceneName: "Level1");
+    }
+    public static void ToLevel2()
+    {
+        SceneManager.LoadScene(sceneName: "Level2");
+    }
+    public static void ToLevel3()
+    {
+        SceneManager.LoadScene(sceneName: "Level3");
+    }
+    public static void ToLevel4()
+    {
+        SceneManager.LoadScene(sceneName: "Level4");
+    }
+    public static void ToLevel5()
+    {
+        SceneManager.LoadScene(sceneName: "Level5");
+    }
+
+    public static void ExitGame()
+    {
+        if (Application.isPlaying)
+        {
+            Application.Quit(exitCode: 0);
+        }
+    }
+
+
 
 }
