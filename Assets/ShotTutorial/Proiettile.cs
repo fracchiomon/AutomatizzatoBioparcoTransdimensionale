5fbde6f81b8dff015c3d48d55069c2082dacf5ea
Arianna4 <136463314+Arianna4@users.noreply.github.com>
2023-09-07 14:18:34 +0200

    merge package locale

---
+++ Assets/ShotTutorial/Proiettile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proiettile : MonoBehaviour {
+
+    public Rigidbody rig;
+    public float speed=100f;
+
+    void Start () {
+		
+	}
+	
+
+	void Update () {
+        rig.AddForce(transform.forward* speed);
+	}
+}
