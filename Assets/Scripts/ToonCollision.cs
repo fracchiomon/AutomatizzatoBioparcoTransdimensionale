660c01b64dc6c9b4c73cca39ca9197b20f4940e1
Arianna4 <136463314+Arianna4@users.noreply.github.com>
2023-09-07 14:18:34 +0200

    merge package locale

---
+++ Assets/Scripts/ToonCollision.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToonCollision : MonoBehaviour
+{
+    
+
+    /*
+    //questo script function is going to send trigger events
+    void OnTriggerEnter(Collider col)
+    {
+        //here we want to add points
+        switch (col.tag) 
+        {
+            case "Do":
+                 break;
+
+            case "Re":
+                break;
+
+            case "Mi":
+                break;
+
+            case "Fa":
+                break;
+
+            case "Sol":
+                break;
+
+            case "La":
+                break;
+
+            case "Si":
+                break;
+
+
+
+        }
+    }
+
+    //this script function is going to send collision events
+    private void OnCollisionEnter(Collision col)
+    {
+        //HERE WE WANT TO NOTIFY THE PLAYER THAT THEY DID NOT RECIVE POINTS
+        switch(col.gameObject.tag)
+        {
+            case "Do":
+                break;
+
+            case "Re":
+                break;
+
+            case "Mi":
+                break;
+
+            case "Fa":
+                break;
+
+            case "Sol":
+                break;
+
+            case "La":
+                break;
+
+            case "Si":
+                break;
+
+        }
+    }*/
+}
