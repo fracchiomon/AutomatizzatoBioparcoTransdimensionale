5e410ac36fa8398bee847ebb0dc499e95cb2a841
aizerrrrrr <lucrezia.minnucci@gmail.com>
2023-09-18 17:53:09 +0200

    fixato singleton game manager

--- Assets/UI/CookingNotes/UI_scripts/UI_MenuButton.cs
+++ Assets/UI/CookingNotes/UI_scripts/UI_MenuButton.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class UI_MenuButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    /*void Start()
-    {
-        
-    }*/
 
     public void OnClick()
     {
         this.GetComponent<Button>().interactable = false;
+        GiocoManager.Instance.ToMainMenu();
     }
 
 }
