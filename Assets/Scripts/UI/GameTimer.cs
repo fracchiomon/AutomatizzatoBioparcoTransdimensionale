b870a4441cdfcf2faac8225486c6a9081925d675
Arianna4 <136463314+Arianna4@users.noreply.github.com>
2023-09-19 15:04:02 +0200

    animazioni scene di fine minigiochi

--- Assets/Scripts/UI/GameTimer.cs
+++ Assets/Scripts/UI/GameTimer.cs
@@ -1,33 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.Windows;
 
 public class GameTimer : MonoBehaviour
 {
-
     Image fillableBar;
     [SerializeField] private float maxTime ;
-    private int punteggioTot;
     [SerializeField] private int punteggioVincita;
-    private UI_Punt punteggioFinale;
-
 
     private static float timeLeft;
-  
 
-
-    // Start is called before the first frame update
     void Start()
     {
         fillableBar = GetComponent<Image>();
         timeLeft = maxTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
      
@@ -39,19 +27,15 @@ public class GameTimer : MonoBehaviour
       
         if (timeLeft < 0 && UI_Punt.Punteggio()<punteggioVincita)
         {
-
             Debug.Log("tempo finito");
 
             Time.timeScale = 0;                     //quando la barra finisce termina il gioco
 
             SceneManager.LoadScene(sceneName: "Lose");
         }
-        else if (UI_Punt.Punteggio() > punteggioVincita)
+        else if (timeLeft < 0 && UI_Punt.Punteggio() > punteggioVincita)
         {
             SceneManager.LoadScene(sceneName: "Victory");
         }
-
     }
-
-
 }
