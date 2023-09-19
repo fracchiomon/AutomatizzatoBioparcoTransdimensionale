64d74da9d77ca432bb9b438ea65d3aecec183570
Arianna4 <136463314+Arianna4@users.noreply.github.com>
2023-09-19 16:57:34 +0200

    CrossHair

--- Assets/Scripts/NoteHunt/Crosshair.cs
+++ Assets/Scripts/NoteHunt/Crosshair.cs
@@ -3,43 +3,35 @@ using UnityEngine;
 public class Crosshair : MonoBehaviour
 {
     [SerializeField] private Transform PlayerTransform;
-    [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Texture2D CrossHair;
+
+    private Camera cam;
                      
-    [SerializeField] private float MuzzleFlashSpeed;                    //fiamma sparata velocità
+
 
     Vector3 pos;
-    Vector3 mouseWorldPosition;
+
+    //Parte aggiunta 
+    private void Start()
+    {
+        cam = Camera.main;
+    }
 
 
+    private void OnEnable()
+    {
+        Vector2 v = new Vector2(CrossHair.width / 2, CrossHair.height / 2);
+        Cursor.SetCursor(CrossHair, v, CursorMode.Auto);
+    }
 
     private void Update()
     {
         pos = Input.mousePosition;
-        Vector2 v = new Vector2(pos.x - CrossHair.width/2, pos.y - CrossHair.height/2);
-        Cursor.SetCursor(CrossHair, v, CursorMode.Auto);
-
+        pos.z = cam.nearClipPlane;
         Vector3 difference = Camera.main.ScreenToWorldPoint(pos) - transform.position;
 
-
-            var bullet = Instantiate(bulletPrefab, mouseWorldPosition, Quaternion.Euler(0,90,0), this.transform);
-            bullet.SetActive(true);
-
-
-
-            Ray ray = Camera.main.ScreenPointToRay(pos);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Nota") && hit.collider.CompareTag("Menu"))
-            {
-                Debug.Log("RAYCAST");
-
-                hit.collider.GetComponent<MoveNota>().setColpito();
-            }
-
-            Destroy(bullet, MuzzleFlashSpeed);
-
     }
+
 
     public void OnMouseDown()
     {
